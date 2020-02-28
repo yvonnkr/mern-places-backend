efab922713d1118e1e28d8d13676357ecd58e0b2
Yvonne Nkr <yvonnkr86@gmail.com>
2020-02-28 16:17:52 +0000

    Added update and delete place routes

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,5 +1,5 @@
 const { v4: uuidV4 } = require("uuid");
-const { DUMMY_PLACES } = require("../data/dummy-places");
+let { DUMMY_PLACES } = require("../data/dummy-places");
 const HttpError = require("../models/http-error");
 
 // single place
@@ -53,4 +53,46 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-module.exports = { getUserPlaces, getPlaceById, createPlace };
+//update place
+const updatePlaceById = (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = DUMMY_PLACES.find(p => p.id === placeId);
+  const index = DUMMY_PLACES.findIndex(p => p.id === placeId);
+
+  if (!place) {
+    const error = new HttpError("Place with given id not found", 404);
+    return next(error);
+  }
+
+  const { title, description, imageUrl } = req.body;
+
+  place.title = title;
+  place.description = description;
+  place.imageUrl = imageUrl;
+
+  DUMMY_PLACES[index] = place;
+
+  res.status(200).json({ place });
+};
+
+//delete place
+const deletePlaceById = (req, res, next) => {
+  const newPlaces = DUMMY_PLACES.filter(p => p.id !== req.params.placeId);
+
+  if (newPlaces.length === 0) {
+    const error = new HttpError("Place with given id not found", 404);
+    return next(error);
+  }
+
+  DUMMY_PLACES = newPlaces;
+
+  res.status(200).json({ message: "Place Deleted", places: DUMMY_PLACES });
+};
+
+module.exports = {
+  getUserPlaces,
+  getPlaceById,
+  createPlace,
+  updatePlaceById,
+  deletePlaceById
+};
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -9,6 +9,13 @@ router.get("/:placeId", placesController.getPlaceById);
 //all user places
 router.get("/user/:uid", placesController.getUserPlaces);
 
+//create new place
 router.post("/", placesController.createPlace);
+
+//update place
+router.patch("/:placeId", placesController.updatePlaceById);
+
+//delete place
+router.delete("/:placeId", placesController.deletePlaceById);
 
 module.exports = router;
